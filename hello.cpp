d6b319b00926607e9452a5c5758773dff2f6db01
Kirk Brauer <kbrauer@one.ai>
2023-09-25 19:09:58 -0400

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    std::coud << "Hello This is a test of the DSA VCG-2 Telematics Unit!";
+    return 0;
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!");
+    println!("This is a test of the DSA VCG-2 Telematics Unit!");
+}
